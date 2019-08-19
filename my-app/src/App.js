a41c83a40583f0bc09061f7a86f88f33c5f3a906
Eddie Callejas <eddiecallejas@hotmail.com>
2019-08-19 12:23:22 -0500

    backend folder and routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,14 +18,14 @@ class App extends Component {
             <a className="navbar-brand" href="https://edcall.github.io/Updated-Portfolio/" target="https://edcall.github.io/Updated-Portfolio/">
               <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
             </a>
-            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <Link to="/" className="navbar-brand">Garage Sale App</Link>
             <div className="collpase nav-collapse">
               <ul className="navbar-nav mr-auto">
                 <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
+                  <Link to="/" className="nav-link">Items</Link>
                 </li>
                 <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
+                  <Link to="/create" className="nav-link">Upload Items</Link>
                 </li>
               </ul>
             </div>
