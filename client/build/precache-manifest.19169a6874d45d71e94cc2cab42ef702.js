6d3cc4f20da6627f1922fca20e599ad36570ad4c
Eddie Callejas <eddiecallejas@hotmail.com>
2019-08-21 12:33:07 -0500

    eslint

---
+++ client/build/precache-manifest.19169a6874d45d71e94cc2cab42ef702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d7512c8ee2768caf52d306598359d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61f641263ed5f7a3f9e3",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "aefa8cfe2427c036a161",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "61f641263ed5f7a3f9e3",
+    "url": "/static/js/2.e66eb615.chunk.js"
+  },
+  {
+    "revision": "aefa8cfe2427c036a161",
+    "url": "/static/js/main.7f185e15.chunk.js"
+  },
+  {
+    "revision": "7b6f2350739161096299",
+    "url": "/static/js/runtime~main.f7c36e2a.js"
+  },
+  {
+    "revision": "6394b7c0e9b9dbfe2262eb146a06c4b7",
+    "url": "/static/media/logo.6394b7c0.png"
+  }
+]);
